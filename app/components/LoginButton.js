05da550d1660a8918383d6720136b4ea6744d365
Rocien Nkunga <rociennkunga@gmail.com>
2024-04-15 14:08:02 -0400

    fixed login button style

--- app/components/LoginButton.js
+++ app/components/LoginButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const LoginButton = () => {
   return (
     <div className="py-4 px-6 flex flex-row justify-center">
-      <button className=" rounded-2xl bg-blue-500 text-slate-300 hover:text-white py-1 px-4 my-6 text-3xl">
+      <button className=" rounded-2xl bg-blue-500 text-slate-300 hover:text-white py-1 px-4 my-6 text-2xl md:text-3xl">
         Login with google
       </button>
     </div>
