4f7a56696ef50949a0b3e5e434581c28ed8f077a
Rocien Nkunga <rociennkunga@gmail.com>
2024-04-15 13:57:24 -0400

    Added button login

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -3,16 +3,19 @@ import Link from 'next/link';
 export default function Navbar({ user }) {
   return (
     <nav class="p-4 bg-gray-600 text-white flex flex-row justify-evenly flex-nowrap">
-      <span className=" underline text-slate-300 hover:text-white md:text-2xl">
+      <span className=" p-1 underline text-slate-300 hover:text-white md:text-2xl">
         <a href="">Search 4 Crap</a>
       </span>
-      <span className=" underline text-slate-300 hover:text-white md:text-2xl">
+      <span className=" p-1 underline text-slate-300 hover:text-white md:text-2xl">
         <a href="">Offer Crap</a>
       </span>
-      <span className=" underline text-slate-300 hover:text-white md:text-2xl">
+      <span className=" p-1 underline text-slate-300 hover:text-white md:text-2xl">
         <a href="">My Crap</a>
       </span>
-      <span className=" underline text-slate-300 hover:text-white md:text-2xl">
+      <span className=" p-1 underline text-slate-300 hover:text-white md:text-2xl">
+        <a href="">Wiped</a>
+      </span>
+      <span className=" p-1 underline text-slate-300 hover:text-white md:text-2xl">
         <a href="">Logout</a>
       </span>
     </nav>
--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import { LoginButton } from './components/LoginButton';
 
 export default function Home() {
-  return <main className="flex flex-col items-center justify-between"></main>;
+  return (
+    <main className="flex flex-col items-center justify-between">
+      <LoginButton />
+    </main>
+  );
 }
